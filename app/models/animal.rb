4762d7384569f9ab97c60e00cfa78e2cd855d68a
Jonathan Mast <jon@padorpers.com>
2015-09-16 15:16:17 -0400

    Add progeny associatin

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,4 +2,12 @@ class Animal < ActiveRecord::Base
   belongs_to :customer
   belongs_to :sire, class_name: 'Animal'
   belongs_to :dam, class_name: 'Animal'
+
+  def progeny
+    if gender == 'M'
+      Animal.where(sire: self)
+    else
+      Animal.where(dam: self)
+    end
+  end
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,16 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Animal do
-  it 'allows setting a sire' do
-    sire = FactoryGirl.create(:animal, gender: 'M')
-    progeny = FactoryGirl.create(:animal, sire: sire)
-    expect(progeny).to be_valid
-    expect(progeny.sire).to eq sire
-  end
-  it 'allows setting a dam' do
-    dam = FactoryGirl.create(:animal, gender: 'F')
-    progeny = FactoryGirl.create(:animal, dam: dam)
-    expect(progeny).to be_valid
-    expect(progeny.dam).to eq dam
+  describe 'sire and dam associations' do
+    it 'allows setting a sire' do
+      sire = FactoryGirl.create(:animal, gender: 'M')
+      progeny = FactoryGirl.create(:animal, sire: sire)
+      expect(progeny).to be_valid
+      expect(progeny.sire).to eq sire
+    end
+    it 'allows setting a dam' do
+      dam = FactoryGirl.create(:animal, gender: 'F')
+      progeny = FactoryGirl.create(:animal, dam: dam)
+      expect(progeny).to be_valid
+      expect(progeny.dam).to eq dam
+    end
+
+    it 'has progeny for sires' do
+      sire = FactoryGirl.create(:animal, gender: 'M')
+      progeny = FactoryGirl.create(:animal, sire: sire)
+      expect(sire.progeny).to include progeny
+    end
+
+    it 'has progeny for dams' do
+      dam = FactoryGirl.create(:animal, gender: 'F')
+      progeny = FactoryGirl.create(:animal, dam: dam)
+      expect(dam.progeny).to include progeny
+    end
   end
 end
