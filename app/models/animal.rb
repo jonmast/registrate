ea30a352570b75c51973d57ba947139cde328d01
Jonathan Mast <jon@padorpers.com>
2015-09-16 20:55:46 -0400

    Add validations

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,11 +3,38 @@ class Animal < ActiveRecord::Base
   belongs_to :sire, class_name: 'Animal'
   belongs_to :dam, class_name: 'Animal'
 
+  enum gender: { 'Male' => 'M', 'Female' => 'F' }
+  enum embryo_transfer: {
+    'None' => 'N',
+    'Domestic' => 'D',
+    'International' => 'I'
+  }
+
+  validate :sire_is_male
+  validate :dam_is_female
+  validates :birth_date,
+            :gender,
+            :birth_type,
+            :embryo_transfer,
+            :percentage,
+            presence: true
+  validates :percentage, inclusion: (0..100)
+
   def progeny
-    if gender == 'M'
+    if gender == 'Male'
       Animal.where(sire: self)
     else
       Animal.where(dam: self)
     end
+  end
+
+  private
+
+  def sire_is_male
+    errors.add(:sire, 'cannot be female') if sire && sire.gender == 'Female'
+  end
+
+  def dam_is_female
+    errors.add(:dam, 'cannot be male') if dam && dam.gender == 'Male'
   end
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -27,4 +27,33 @@ RSpec.describe Animal do
       expect(dam.progeny).to include progeny
     end
   end
+
+  describe 'validations' do
+    let(:sheep) { FactoryGirl.build(:animal) }
+
+    it 'requires gender to be M or F' do
+      expect { sheep.gender = 'f' }.to raise_error
+    end
+
+    it "doesn't allow setting embryo_transfer to z" do
+      expect { sheep.embryo_transfer = 'z' }.to raise_error
+    end
+
+    it 'requires sires to be male' do
+      female_sire = FactoryGirl.create(:animal, gender: 'F')
+      sheep.sire = female_sire
+      expect(sheep).to_not be_valid
+    end
+
+    it 'requires dams to be female' do
+      male_dam = FactoryGirl.create(:animal, gender: 'M')
+      sheep.dam = male_dam
+      expect(sheep).to_not be_valid
+    end
+
+    it 'requires percentage to be between 0 and 100' do
+      sheep.percentage = 101
+      expect(sheep).to_not be_valid
+    end
+  end
 end
