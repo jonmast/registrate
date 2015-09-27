8ac4fd441282b2f3b591974c39bedbe47c9e0928
Jonathan Mast <jon@padorpers.com>
2015-09-27 19:14:03 -0400

    Add user info page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   devise_for :users
   resources :animals, only: [:show, :new, :create]
+  resources :users, only: [:show]
 end
---
+++ spec/features/show_users_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature 'ShowUsers' do
+  scenario 'user info page' do
+    user = FactoryGirl.create(:user)
+    animal = FactoryGirl.create(:animal, owner: user)
+    visit user_path(user)
+    within('#attributes') do
+      expect(page).to have_content user.name
+    end
+    expect(page).to have_link(animal.registration_id, href: animal_path(animal))
+  end
+end
