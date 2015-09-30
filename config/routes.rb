37a4012bdf87b451fe68d5996aabaffcbcdb179f
Jonathan Mast <jon@padorpers.com>
2015-09-30 09:12:31 -0400

    User dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    root 'users#dashboard', as: :dashboard
+  end
   root 'static_pages#home'
   devise_for :users
   resources :animals, only: [:show, :new, :create]
---
+++ spec/features/user_dashboards_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature 'UserDashboards' do
+  scenario 'show dashboard upon signing in' do
+    user = sign_in
+    owned_animal = FactoryGirl.create(:animal, owner: user)
+    bred_animal = FactoryGirl.create(:animal, breeder: user)
+    visit root_path
+    expect(page).to have_content user.name
+    expect(page).to have_link owned_animal.registration_id, animal_path(owned_animal)
+    expect(page).to_not have_link bred_animal.registration_id, animal_path(bred_animal)
+  end
+end
