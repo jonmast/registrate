fe7ab7d7b442332bbe32d263acbb6750fac1cba2
Jonathan Mast <jon@padorpers.com>
2015-10-03 11:06:30 -0400

    Improve list appearance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,10 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!, except: :show
   def show
     @user = User.find(params[:id])
+    @animals = @user.owned_animals.page(params[:page])
   end
 
   def dashboard
-
+    @animals = current_user.owned_animals.page(params[:page])
   end
 end
