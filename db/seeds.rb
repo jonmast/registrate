07664343b83107a2d0cd1d6528f5216bcfab19da
Jonathan Mast <jon@padorpers.com>
2015-10-11 14:51:20 -0400

    Add some random sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
+User.destroy_all
 Animal.destroy_all
 
-FactoryGirl.create(:animal_with_parents)
+users = FactoryGirl.build_list(:user, 10).map do |user|
+  user.skip_confirmation!
+  user.save!
+  user
+end
+FactoryGirl.create_list(:ram, 5, owner: users[0], breeder: users[0], birth_date: 20.years.ago)
+FactoryGirl.create_list(:ewe, 5, owner: users[0], breeder: users[0], birth_date: 20.years.ago)
+
+200.times do
+  sire = Animal.where(gender: 'M').order('RANDOM()').first
+  dam = Animal.where(gender: 'F').order('RANDOM()').first
+  birth_date = [sire.birth_date, dam.birth_date].max + 1.year + 5.months
+  FactoryGirl.create(:animal,
+                     sire: sire,
+                     dam: dam,
+                     breeder: dam.owner,
+                     birth_date: birth_date,
+                     owner: users.sample
+                    )
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
+    flock_name { Faker::Lorem.word.capitalize + [' Farm', ' Ranch'].sample }
     name { Faker::Name.name }
     password 'supersecret'
     password_confirmation 'supersecret'
@@ -7,7 +8,6 @@ FactoryGirl.define do
     city { Faker::Address.city }
     state { Faker::Address.state_abbr }
     zip { Faker::Address.zip }
-    sequence(:flock_name) { |n| "Flock #{n}" }
     email { Faker::Internet.email }
     phone { Faker::PhoneNumber.phone_number }
   end
