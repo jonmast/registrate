0533706d226f0a457fac3c6a9a7bf08b70e6d251
Jonathan Mast <jon@padorpers.com>
2015-10-16 16:03:15 -0400

    Use Time.zone.now instead of Time.now
    Time.now uses the system timezone which can be different from Rails's
    timezone.

--- app/helpers/animals_helper.rb
+++ app/helpers/animals_helper.rb
@@ -1,8 +1,8 @@
 module AnimalsHelper
   def age_in_words(birth_date)
-    months = (Time.now.year - birth_date.year) * 12 + (Time.now.month - birth_date.month)
+    months = (Time.zone.now.year - birth_date.year) * 12 + (Time.zone.now.month - birth_date.month)
     years, months = months.divmod(12)
-    days = Time.now.day - birth_date.day
+    days = Time.zone.now.day - birth_date.day
     months -= 1 if days < 0
     if months < 0
       years -= 1
